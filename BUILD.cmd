84a2892703284441bb297c5db546322be167b067
kpreisser <kpreisser@users.noreply.github.com>
2015-10-26 22:36:23 +0100

    Fix the build script.

--- BUILD.cmd
+++ BUILD.cmd
@@ -1,9 +1,8 @@
 @echo off
-if not "%ProgramFiles(x86)%"=="" (
-    set ProgramFiles32Bit=%ProgramFiles(x86)%
-) else (
-	set ProgramFiles32Bit=%ProgramFiles%
-)
+REM I didn't use the if (...) else (...) form because when the environment variable
+REM is expanded, the ")" in the directory name seems to be interpreted by the IF command.
+if not "%ProgramFiles(x86)%"=="" set ProgramFiles32Bit=%ProgramFiles(x86)%
+if "%ProgramFiles(x86)%"=="" set ProgramFiles32Bit=%ProgramFiles%
 set BuildExe=%ProgramFiles32Bit%\MSBuild\14.0\Bin\MSBuild.exe
 
 if not exist "%BuildExe%" (
