331c5eaa7bb54db3aa47d72f289cf288f7162a78
kpreisser <kpreisser@users.noreply.github.com>
2016-04-30 16:36:32 +0200

    Add a fishing function for Lullaby Lane, Donald's Dreamland. Fixes #8

--- TTMouseclickSimulator/MainWindow.xaml.cs
+++ TTMouseclickSimulator/MainWindow.xaml.cs
@@ -1,42 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Xml.Serialization;
+
 using TTMouseclickSimulator.Core;
 using TTMouseclickSimulator.Core.Actions;
 using TTMouseclickSimulator.Core.Environment;
-using TTMouseclickSimulator.Core.ToontownRewritten.Actions;
-using TTMouseclickSimulator.Core.ToontownRewritten.Actions.DoodleInteraction;
-using TTMouseclickSimulator.Core.ToontownRewritten.Actions.Fishing;
-using TTMouseclickSimulator.Core.ToontownRewritten.Actions.Keyboard;
-using TTMouseclickSimulator.Core.ToontownRewritten.Actions.Speedchat;
 using TTMouseclickSimulator.Core.ToontownRewritten.Environment;
 using TTMouseclickSimulator.Project;
 
 namespace TTMouseclickSimulator
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
 
