960eba1a6a197d5e6a229456be318818778b1b7e
kpreisser <kpreisser@users.noreply.github.com>
2016-05-17 19:55:01 +0200

    Follow-Up.

--- TTMouseclickSimulator/Utils/TaskDialog.cs
+++ TTMouseclickSimulator/Utils/TaskDialog.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Interop;
 using WinFormsIWin32Window = System.Windows.Forms.IWin32Window;
